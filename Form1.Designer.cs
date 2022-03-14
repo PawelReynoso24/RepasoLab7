7cc09762cf27bc76cc7db9bcf49ff902d3454fc4
Pawel Alessandro Reynoso Marquez <pawelreynoso@gmail.com>
2022-03-14 17:07:03 -0600

    Formulario inicial

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonMostrarDatos = new System.Windows.Forms.Button();
+            this.buttonOrdenarDatos = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(39, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(470, 223);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // buttonMostrarDatos
+            // 
+            this.buttonMostrarDatos.Location = new System.Drawing.Point(64, 313);
+            this.buttonMostrarDatos.Name = "buttonMostrarDatos";
+            this.buttonMostrarDatos.Size = new System.Drawing.Size(130, 56);
+            this.buttonMostrarDatos.TabIndex = 1;
+            this.buttonMostrarDatos.Text = "Mostrar Resumen";
+            this.buttonMostrarDatos.UseVisualStyleBackColor = true;
+            // 
+            // buttonOrdenarDatos
+            // 
+            this.buttonOrdenarDatos.Location = new System.Drawing.Point(229, 313);
+            this.buttonOrdenarDatos.Name = "buttonOrdenarDatos";
+            this.buttonOrdenarDatos.Size = new System.Drawing.Size(130, 56);
+            this.buttonOrdenarDatos.TabIndex = 2;
+            this.buttonOrdenarDatos.Text = "Ordenar Datos";
+            this.buttonOrdenarDatos.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonOrdenarDatos);
+            this.Controls.Add(this.buttonMostrarDatos);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonMostrarDatos;
+        private System.Windows.Forms.Button buttonOrdenarDatos;
     }
 }
 
